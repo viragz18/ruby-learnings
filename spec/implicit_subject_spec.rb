a6a1710fdb0c2f9473ab23bc56654e76c7bf2918
viragz18 <viragz18@gmail.com>
2020-04-09 09:53:33 -0400

    added example in implicit subject

--- spec/implicit_subject_spec.rb
+++ spec/implicit_subject_spec.rb
@@ -3,5 +3,12 @@ RSpec.describe Hash do
         # puts subject
         # puts subject.class
         expect(subject.length).to eq (0)
+        subject[:some_key] = "some val"
+        expect(subject.length).to eq(1)
     end
+
+    it 'is isolated between examples' do
+        expect(subject.length).to eq(0)
+    end   
+
 end
