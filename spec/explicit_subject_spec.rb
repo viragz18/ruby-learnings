835c8ec9aeaef5c7353f7eb04bc8b29ddc476fa6
Virag <viragz18@gmail.com>
2020-04-12 18:29:48 -0400

    add explicit subject

--- spec/explicit_subject_spec.rb
+++ spec/explicit_subject_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Hash do
         expect(subject.length).to eq(2)
         expect(bob.length).to eq(2)
     end
+
+    it "is isolated between tests" do
+        expect(bob.length).to eq(2)
+    end
+
+    describe 'nested example' do
+        it 'has 2 key value pairs' do
+            expect(bob.length).to eq(2)
+        end
+    end
+
 end
