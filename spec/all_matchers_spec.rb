4bc0e80c2c9eb8dc17d0d708e95196d8d42a25e5
Virag <viragz18@gmail.com>
2020-04-19 16:25:49 -0400

    all matchers

---
+++ spec/all_matchers_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe 'all matchers' do
+    it 'allows for aggregare checks' do
+        [5,7,9].each do |val|
+            expect(val).to be_odd
+        end
+
+        expect([5,7,9]).to all(be_odd)
+
+        expect([2,4,6]).to all(be_even)
+
+        expect([[],[],[],[]]).to all(be_empty)
+
+        expect([0,0,0]).to all(be_zero)
+
+        expect([5,7,9,]).to all(be<10)
+    end
+
+    describe [5,7,9] do
+        it { is_expected.to all(be_odd) }
+        it { is_expected.to all(be<10)}
+    end
+end
