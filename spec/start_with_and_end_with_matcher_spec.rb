2861f64e587013a4dcd6175d04f458e8aafe0b1c
Virag <viragz18@gmail.com>
2020-04-19 17:11:25 -0400

    start_with and end_with matchers

---
+++ spec/start_with_and_end_with_matcher_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'start_with and end_with matcher' do
+    describe 'caterpillar' do
+        it { is_expected.to start_with('cat')}
+        it { is_expected.to end_with('pillar')}
+        #it { is_expected.to end_with('Pillar')} will fail due to case sensitivity
+    end
+
+    describe [:a, :b, :c, :d] do
+        it { is_expected.to start_with(:a)}
+        it { is_expected.to start_with(:a, :b)}
+        it { is_expected.to start_with(:a, :b, :c)}
+        it { is_expected.to end_with(:c, :d)}
+    end
+
+end
